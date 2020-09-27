74a5aefe01b3ff4f3755649d4ea88ce71526a810
jiwomdf <mdf.jiwo@outlook.com>
2020-09-27 22:00:49 +0700

    add ShoppingViewModelTest.kt for ShoppingViewModel.kt

--- app/src/main/java/com/programmergabut/android_jetpack_testing/ui/ShoppingViewModel.kt
+++ app/src/main/java/com/programmergabut/android_jetpack_testing/ui/ShoppingViewModel.kt
@@ -7,10 +7,12 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.programmergabut.android_jetpack_testing.data.local.ShoppingItem
 import com.programmergabut.android_jetpack_testing.data.remote.responses.ImageResponse
+import com.programmergabut.android_jetpack_testing.other.Constants
 import com.programmergabut.android_jetpack_testing.other.Event
 import com.programmergabut.android_jetpack_testing.other.Resource
 import com.programmergabut.android_jetpack_testing.repositories.ShoppingRepository
 import kotlinx.coroutines.launch
+import kotlin.Exception
 
 class ShoppingViewModel @ViewModelInject constructor(
     private val repository: ShoppingRepository
@@ -42,11 +44,43 @@ class ShoppingViewModel @ViewModelInject constructor(
     }
 
     fun insertShoppingItem(name: String, amountStr: String, priceStr: String){
+        if(name.isEmpty() || amountStr.isEmpty() || priceStr.isEmpty()){
+            _insertShoppingItemStatus.postValue(Event(Resource.error("The field must not be empty", null)))
+            return
+        }
+        if(name.length > Constants.MAX_NAME_LENGTH){
+            _insertShoppingItemStatus.postValue(Event(Resource.error("The name of the item must not exceed " +
+                    "${Constants.MAX_NAME_LENGTH} characters", null)))
+            return
+        }
+        if(priceStr.length > Constants.MAX_PRICE_LENGTH){
+            _insertShoppingItemStatus.postValue(Event(Resource.error("The price of the item must not exceed " +
+                    "${Constants.MAX_PRICE_LENGTH} characters", null)))
+            return
+        }
+        val amount = try {
+            amountStr.toInt()
+        }catch (e: Exception){
+            _insertShoppingItemStatus.postValue(Event(Resource.error("please insert a valid amount", null)))
+            return
+        }
 
+        val shoppingItem = ShoppingItem(name, amount, priceStr.toFloat(), _curImageUrl.value ?: "")
+        insertShoppingItemIntoDB(shoppingItem)
+        setCurImageUrl("")
+        _insertShoppingItemStatus.postValue(Event(Resource.success(shoppingItem)))
     }
 
     fun searchForImage(imageQuery: String){
+        if(imageQuery.isEmpty()){
+            return
+        }
+        _images.value = Event(Resource.loading(null))
 
+        viewModelScope.launch {
+            val response = repository.searchForImage(imageQuery)
+            _images.value = Event(response)
+        }
     }
 
 
