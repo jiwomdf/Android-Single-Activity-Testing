0afcbaa924b0799ff2bfcdc7ad65d59b24f87190
jiwomdf <mdf.jiwo@outlook.com>
2020-09-29 09:26:33 +0700

    add HiltExt.kt and HiltTestActivity.kt for testing fragment

---
+++ app/src/debug/java/com/programmergabut/android_jetpack_testing/HiltTestActivity.kt
@@ -0,0 +1,7 @@
+package com.programmergabut.android_jetpack_testing
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity : AppCompatActivity()
